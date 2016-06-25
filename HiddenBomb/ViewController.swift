6d57c46ca46960f41e1b5b4f8e93733b44c9f286
PreviousHippo <hty_1992@hotmail.com>
2016-06-25 06:25:47 -0600

    Initial Commit

---
+++ HiddenBomb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiddenBomb
+//
+//  Created by Andy Hu on 2016-06-25.
+//  Copyright © 2016 Andy Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
