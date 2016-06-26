55e44a17a391ba2900ccf7fde7a410d3d59618a6
PreviousHippo <hty_1992@hotmail.com>
2016-06-26 00:13:52 -0600

    first iOS app

--- HiddenBomb/ViewController.swift
+++ HiddenBomb/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RedBombImg: UIImageView!
+    @IBOutlet weak var BlueBombImg: UIImageView!
+    @IBOutlet weak var HidRBombButton: UIButton!
+    @IBOutlet weak var HidBBombButton: UIButton!
+    @IBOutlet weak var Reset: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideRed(sender: AnyObject) {
+        RedBombImg.hidden = true
+        BlueBombImg.hidden = false
+        HidRBombButton.hidden = true
+        HidBBombButton.hidden = true
+    }
 
+    @IBAction func HideBlue(sender: AnyObject) {
+        RedBombImg.hidden = false
+        BlueBombImg.hidden = true
+        HidRBombButton.hidden = true
+        HidBBombButton.hidden = true
+    }
+
+    @IBAction func Reset(sender: AnyObject) {
+        RedBombImg.hidden = false
+        BlueBombImg.hidden = false
+        HidRBombButton.hidden = false
+        HidBBombButton.hidden = false
+    }
 }
 
